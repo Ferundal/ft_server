e3c2a56302f8229ba2c3aa556f9c5280e7997cd2
ferundal <ferundal@gmail.com>
2021-04-27 14:30:41 +0300

    all done 2

--- srcs/start_serv.sh
+++ srcs/start_serv.sh
@@ -4,9 +4,9 @@ service mysql start
 service php7.3-fpm start
 
 # Configure a wordpress database
-echo "CREATE DATABASE wordpress;"| mysql -u root --skip-password
-echo "GRANT ALL PRIVILEGES ON wordpress.* TO 'root'@'localhost' WITH GRANT OPTION;"| mysql -u root --skip-password
-echo "FLUSH PRIVILEGES;"| mysql -u root --skip-password
-echo "update mysql.user set plugin='' where user='root';"| mysql -u root --skip-password
+mysql -u root --skip-password -e "CREATE DATABASE wordpress;"
+mysql -u root --skip-password -e "CREATE USER 'admin'@'localhost' IDENTIFIED BY 'admin';"
+mysql -u root --skip-password -e "GRANT ALL PRIVILEGES ON wordpress.* to 'admin'@'localhost';"
+mysql -u root --skip-password -e "FLUSH PRIVILEGES;"
 
 nginx -g 'daemon off;'
